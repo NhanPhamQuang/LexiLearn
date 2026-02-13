e6b5ca08c709b8c7825a90f4ecd20f5f6af15582
Nhan Pham Quang <nhanphamquang196@gmail.com>
2026-02-14 00:31:54 +0700

    initial deploy

--- app/main.py
+++ app/main.py
@@ -4,8 +4,6 @@ from app.routes import web
 
 app = FastAPI()
 
-# Mount static folder
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
-# Include routes
 app.include_router(web.router)
--- app/routes/web.py
+++ app/routes/web.py
@@ -5,14 +5,41 @@ from fastapi.responses import HTMLResponse
 router = APIRouter()
 templates = Jinja2Templates(directory="app/templates")
 
+
 @router.get("/", response_class=HTMLResponse)
 def home(request: Request):
     return templates.TemplateResponse("home.html", {"request": request})
+
+
+@router.get("/platform", response_class=HTMLResponse)
+def platform(request: Request):
+    return templates.TemplateResponse("platform.html", {"request": request})
+
+
+@router.get("/architecture", response_class=HTMLResponse)
+def architecture(request: Request):
+    return templates.TemplateResponse("architecture.html", {"request": request})
+
+
+@router.get("/about", response_class=HTMLResponse)
+def about(request: Request):
+    return templates.TemplateResponse("about.html", {"request": request})
+
 
 @router.get("/quiz", response_class=HTMLResponse)
 def quiz(request: Request):
     return templates.TemplateResponse("quiz.html", {"request": request})
 
+
 @router.get("/result", response_class=HTMLResponse)
 def result(request: Request):
     return templates.TemplateResponse("result.html", {"request": request})
+
+@router.get("/signin", response_class=HTMLResponse)
+def signin(request: Request):
+    return templates.TemplateResponse("signin.html", {"request": request})
+
+
+@router.get("/signup", response_class=HTMLResponse)
+def signup(request: Request):
+    return templates.TemplateResponse("signup.html", {"request": request})
